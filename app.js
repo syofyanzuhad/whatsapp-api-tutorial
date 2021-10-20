e386953ab86fb636a1f5cd8ee3da007f70321be0
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-10-20 10:46:07 +0700

    restore: socketIo

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const { Client, MessageMedia } = require('whatsapp-web.js');
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-// const socketIO = require('socket.io');
-const { Server } = require("socket.io");
+const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const { phoneNumberFormatter } = require('./helpers/formatter');
@@ -12,8 +11,7 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-// const io = socketIO(server);
+const io = socketIO(server);
 
 app.use(express.json());
 app.use(express.urlencoded({
